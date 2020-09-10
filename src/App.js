4fd2febdd3c2761ad51fb68767a27bdaca8ab198
rokibulsanto <rhsanto360@gmail.com>
2020-09-10 23:55:31 +0600

    firebase practise done for today

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ firebase.initializeApp(firebaseConfig);
 function App() {
 
   const provider = new firebase.auth.GoogleAuthProvider();
-
+  const [newUser,setNewUser] = useState(false);
   const [user,setUser] = useState({
-    isSignIn:'false',
+    isSignIn: false,
     name:'',
     email:'',
+    password:'',
     photo:''
 
   })
@@ -26,7 +27,7 @@ function App() {
       const {displayName,email,photoURL} = res.user;
       const signInUser = {
 
-        isSignIn:'true',
+        isSignIn: true,
         name:displayName,
         email:email,
         photo:photoURL
@@ -44,10 +45,12 @@ function App() {
     firebase.auth().signOut()
     .then(res => {
       const signOutUser = {
-        isSignIn:'false',
+        isSignIn: false,
         name:'',
         email:'',
-        photo:''
+        photo:'',
+        error:'',
+        success:false
 
       }
       setUser(signOutUser);
@@ -56,6 +59,71 @@ function App() {
       console.log(err);
     })
   }
+const handleBlur = (e) => {
+  let isFieldValid = true;
+  if(e.target.name === 'email'){
+    isFieldValid =  /\S+@\S+\.\S+/.test(e.target.value);
+   
+  }
+  if(e.target.name === 'password'){
+    const isPasswordValid = e.target.value.length > 6;
+    const isPasswordHasNumber = /\d{1}/.test(e.target.value);
+     isFieldValid= isPasswordValid && isPasswordHasNumber;
+  }
+  if(isFieldValid){
+    const newUserInfo = {...user};
+    newUserInfo[e.target.name] = e.target.value;
+    setUser(newUserInfo);
+  }
+}
+const handleSubmit = (e) => {
+  if(newUser && user.email && user.password){
+    firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+    .then(res => {
+      const newUserInfo = {...user};
+      newUserInfo.error = '';
+      newUserInfo.success = true;
+      setUser(newUserInfo);
+      updateUserName(user.name);
+      
+    })
+    .catch(error => {
+        const newUserInfo = {...user};
+        newUserInfo.error =error.message;
+        newUserInfo.success = false;
+        setUser(newUserInfo);
+    });
+  }
+  if(!newUser && user.email && user.password){
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    .then(res => {
+      const newUserInfo = {...user};
+      newUserInfo.error = '';
+      newUserInfo.success = true;
+      setUser(newUserInfo);
+      console.log('sign in user info',res.user);
+    })
+    .catch(function(error) {
+      const newUserInfo = {...user};
+        newUserInfo.error =error.message;
+        newUserInfo.success = false;
+        setUser(newUserInfo);
+    });
+  }
+
+  e.preventDefault();
+}
+ const updateUserName = name => {
+      const user = firebase.auth().currentUser;
+
+      user.updateProfile({
+        displayName: name
+      }).then(function() {
+        console.log('userName update successfully');
+      }).catch(function(error) {
+        console.log(error);
+      });
+ } 
 
   return (
     <div className="App">
@@ -72,6 +140,24 @@ function App() {
           <p>Your email {user.email}</p>
           <img src={user.photo} alt=""/>
         </div>
+      }
+
+      <h1>Our own Authentication</h1>
+      <input type="checkbox" onChange={()=> setNewUser(!newUser)} name="newUser" id=""/>
+      <label htmlFor="newUser">NewUser sign-up</label>
+  
+      <form onSubmit={handleSubmit}>        
+        {newUser && <input type="text" name="name" onBlur={handleBlur} placeholder="Your Name"/>}
+        <br/>
+        <input type="text" name="email" onBlur={handleBlur} placeholder="Your Email address" required/>
+        <br/>
+        <input type="password" name="password" onBlur={handleBlur} placeholder="Your password" required />
+        <br/>
+        <input type="submit" value={newUser ? 'Sign up': 'Sign in'}/>
+      </form>
+      <p style={{color:'red'}}>{user.error}</p>
+      {
+        user.success && <p style = {{color:'green'}}>User {newUser ?'created' : 'Logged In'} successfully</p>
       }
     </div>
   );
